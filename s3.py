ed842177089518a154535e536525d06e6096fddd
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-11 15:57:56 +0330

    sadeghi

---
+++ s3.py
@@ -0,0 +1,43 @@
+# message = "hello every body, welcome to our \"python\" class.\nwe are going to learn everything about python.\npython from zero to hero!!"
+# print(message)
+# message = """hello every body, welcome to our "python" class.
+# we are going to learn everything about "python".
+# python from zero to hero!!"""
+# print(message)
+# message = '''hello every body, welcome to our "python" class.
+# we are going to learn everything about python.
+# python from zero to hero!!'''
+# print(message)
+
+
+# exercise 1 print this statement :  He said, "Aren't can't shouldn't wouldn't."    عبارت مقابل را به همین صورت پرینت نمائید.
+
+# star = '*' * 10
+# print(star)
+
+# name = input('enter your name:> ')
+# course = input('enter the name of your favorite course: ')
+# message = f'''hello {name}, welcome to our "{course}" class.
+# we are going to learn everything about {course}.
+# {course} from zero to hero!!'''
+# print(message)
+
+
+# casting or converting    تبدیل نوع - رشته به عدد صحیح
+# number1 = int(input('enter first number '))
+# number2 = int(input('enter second number '))
+
+# result = number1 + number2
+
+# print(f"sum of {number1} and {number2} is:", result)
+# converting to float number تبدیل به عدد اعشاری
+number1 = float(input('enter first number '))
+number2 = float(input('enter second number '))
+
+result = number1 + number2
+
+print(f"sum of {number1} and {number2} is:", result)
+
+
+# exercise 2 : برنامه ای بنویسید که دو عدد را از ورودی دریافت نماید و حاصل جمع آن ها را به صورت زیر نمایش دهد
+# 10 + 12 = 22
