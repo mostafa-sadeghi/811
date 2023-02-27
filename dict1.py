eb695b61108ebc8d432918a81250d0ee152bcb79
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-27 16:00:38 +0330

    sadeghi

--- dict1.py
+++ dict1.py
@@ -1,9 +1,12 @@
-favorite_sports = {}
+# favorite_sports = {}
 
-favorite_sports['reza'] = 'football'
-favorite_sports['ali'] = 'basketball'
-favorite_sports['mina'] = 'football'
-print(favorite_sports)
+# favorite_sports['reza'] = 'football'
+# favorite_sports['ali'] = 'basketball'
+# favorite_sports['mina'] = 'football'
+# print(favorite_sports)
 
-print("reza likes:")
-print(favorite_sports['reza'])
+# print("reza likes:")
+# print(favorite_sports['reza'])
+
+# print(" " * 2 + "*")
+# print("* " * 3)
---
+++ review1.py
@@ -0,0 +1,34 @@
+# print('Hello every body')
+# print("Hello every body")
+
+# print("1+2*3")
+# print(1+2*3)
+# print((1+2)*3)
+# score = 0
+
+# print("score :", score)
+
+# score = score + 1
+# print("new score :", score)
+# score += 1
+# print("new score :", score)
+
+# score = score - 1
+# print("new score :", score)
+# score -= 1
+# print("new score :", score)
+
+# exe1 : برنامه ای بنویسید که 3 عدد از ورودی دریافت نماید و حاصل عبارات زیر را محاسبه و نمایش دهد
+#  عدد اول + عدد دوم - عددسوم
+#  عدد اول * عدد دوم + عدد سوم
+
+# exe 2:
+# برنامه ای بنویسد که نام، نام خانوادگی و سن شمارا از ورودی دریافت نماید
+# و پیغام زیر را پرینت کند"
+# nima rezaei you are 12 years old.
+
+print("id\tname\tscore")
+print("01\tali\t100")
+print("02\talireza\t90")
+
+# fstring justify
