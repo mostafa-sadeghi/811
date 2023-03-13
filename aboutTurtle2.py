d46ab2b5bff5112af865cef1bf0f0e9bf551edb7
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-13 15:57:06 +0330

    sadeghi

--- aboutTurtle2.py
+++ aboutTurtle2.py
@@ -1,10 +1,31 @@
 import turtle
-
+import random
+# تولید اعداد تصادفی
+# number = random.randrange(9)
+# print(number)
+COLORS = ['red', 'green', 'blue', 'gray', 'cyan', 'gold']
+color_index = random.randrange(6)
 s = turtle.Screen()
 s.bgcolor('black')
-s.bgpic('mario.png')
+# s.bgpic('mario.png')
 p = turtle.Pen()
-p.pencolor('red')
-
-
+p.pensize(4)
+p.pencolor(COLORS[color_index])
+# کشیدن اولین مثلث
+p.fd(100)
+p.right(120)
+p.fd(100)
+p.right(120)
+p.fd(100)
+p.right(120)
+# کشیدن دومین مثلث
+p.fillcolor('green')
+p.begin_fill()
+p.fd(100)
+p.left(120)
+p.fd(100)
+p.left(120)
+p.fd(100)
+p.left(120)
+p.end_fill()
 s.exitonclick()
