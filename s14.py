632e105445aaec973426f6a6f3d5aab55421aaaa
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-03 15:58:22 +0330

    sadeghi

---
+++ s14.py
@@ -0,0 +1,58 @@
+# a = 4
+# b = 5
+# if a < b:
+#     print("a is lower than b.")
+# if a > b:
+#     print("a is greater than b.")
+
+a = 4
+b = 4
+
+# if a <= b:
+#     print("a is lower than or equal to b.")
+
+# if a >= b:
+#     print("a is greater than or equal to b.")
+
+# if a == b:
+#     print("a and b are equal.")
+
+# if a != b:
+#     print("a and b are not equal.")
+# if a==b:
+#     print(f'{a} and {b} are both equal.')
+# else:
+#     print(f'{a} and {b} are not equal.')
+
+# ماشین حساب
+
+# دو عدد را از ورودی دریافت نمائید
+# سپس عملگر را از ورودی دریافت نمائید
+# منظور از عملگر موارد زیر می باشد:
+#  +     -      *      /
+# دستورات مورد نیاز:
+# input()     varaiable    print()    if   else
+
+age = 12
+
+if age < 8:
+    print("kid")
+elif 8 <= age < 13:
+    print("junior")
+
+else:
+    print("adult")
+
+# تمرین 2
+
+# برنامه ای بنویسید که نام و سن فرد را از ورودی دریافت نماید
+# و در صورتی که سن او از هشت کمتر بود
+# kid
+# را پرینت نماید
+# در صورتی که سن او بین 8 تا 13 بود
+# junior را پرینت نماید
+# در صورتی که سن او بین 13 تا 18 بود
+# teenager
+# را پرینت نماید
+# در صورتی که از 18 به بالا بود
+# adult را پرینت نماید
