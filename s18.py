6dd31303394f8e51d9c716c10754387bf678845a
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-24 16:02:06 +0330

    sadeghi

--- s18.py
+++ s18.py
@@ -4,8 +4,8 @@ screen = pygame.display.set_mode((700, 500))
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 screen.fill(WHITE)
-rect_x = 50
-rect_y = 50
+rect_x = 0
+rect_y = 0
 rect_change_x = 1
 rect_change_y = 1
 
@@ -14,12 +14,14 @@ while done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = False
-    pygame.draw.rect(screen, BLACK, [rect_x, rect_y, 50, 50])
+    pygame.draw.rect(screen, BLACK, [rect_x, rect_y, 5, 5])
     rect_x += rect_change_x
     rect_y += rect_change_y
-    if rect_y > 450 or rect_y < 0:
+    if rect_y > 495 or rect_y < 0:
         rect_change_y = rect_change_y * -1
     # exercise 1 : برگشت از گوشه های راست و چپ صفحه
+    if rect_x > 695 or rect_x < 0:
+        rect_change_x = -1 * rect_change_x
     pygame.display.flip()
 
 
