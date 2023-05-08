925d533b22b2a5fa31d72d0986d6fd1a8f4c42bd
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-08 16:03:13 +0330

    sadeghi

---
+++ s23.py
@@ -0,0 +1,9 @@
+Months = "JanFebMarAprMayJunJulAugSepOctNovDec"
+
+# شماره ماه را از ورودی بگیرد
+# و اسم ماه را از رشته بالا جدا کند و نمایش دهد
+# 1   =>     Jan
+# 2   =>     Feb
+# 12  =>     Dec
+# 13  =>     "month number is not correct"
+# 0   =>     "month number is not correct"
